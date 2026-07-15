863b08eec763858c084866f4e3f1d26d746a06bc
Steven Hedges <steven.hedges34@gmail.com>
2026-07-15 03:05:01 -0400

    Add PPU register address constants to Bus.h

--- src/Bus.h
+++ src/Bus.h
@@ -25,17 +25,21 @@ private:
     std::array<std::uint8_t, 2048> ram_{};
     Cartridge& cartridge_;
 
-    // Addresses
+    /// CPU RAM
     static constexpr std::uint16_t RAM_SIZE = 2048; // 2 KB
     static constexpr std::uint16_t RAM_START = 0x0000;
     static constexpr std::uint16_t RAM_END = 0x07FF;
     static constexpr std::uint16_t RAM_MIRROR_END = 0x1FFF;
     static constexpr std::uint16_t RAM_MASK = 0x07FF;
-    // PRG-ROM Cartridge - covers the 32KB of address space for the cartridge code
+    /// PRG-ROM Cartridge - covers the 32KB of address space for the cartridge code
     static constexpr std::uint16_t PRG_ROM_START = 0x8000;
     static constexpr std::uint16_t PRG_ROM_END = 0xFFFF;
+    /// PPU Registers
+    static constexpr std::uint16_t PPU_START = 0x2000;
+    static constexpr std::uint16_t PPU_END = 0x2007; // 8 registers
+    static constexpr std::uint16_t PPU_MIRROR_END = 0x3FFF; // Mirrored every 8 bytes across 8KB
 };
 
-} // nes
+} // namespace nes
 
 #endif //NES_EMULATOR_CPP_BUS_H
