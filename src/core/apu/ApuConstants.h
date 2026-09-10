3c5948baeb962c189b632e53396f6139732fb6b5
Steven Hedges <steven.hedges34@gmail.com>
2026-09-10 00:14:48 -0400

    Fix formatting

--- src/core/apu/ApuConstants.h
+++ src/core/apu/ApuConstants.h
@@ -4,8 +4,7 @@
 namespace ApuConstants {
 
 // Length counter look-up table. Shared by Pulse, Triangle, and Noise:
-inline constexpr uint8_t LENGTH_COUNTER_TABLE[32] = {10, 254, 20,  2,  40, 4,  80, 6,  160, 8,  60,
-                                                          10, 14,  12,  26, 14, 12, 16, 24, 18,  48, 20,
-                                                          96, 22,  192, 24, 72, 26, 16, 28, 32,  30};
+inline constexpr uint8_t LENGTH_COUNTER_TABLE[32] = {10, 254, 20, 2,  40, 4,  80, 6,  160, 8,  60, 10, 14, 12, 26, 14,
+                                                     12, 16,  24, 18, 48, 20, 96, 22, 192, 24, 72, 26, 16, 28, 32, 30};
 
 } // namespace ApuConstants
