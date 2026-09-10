d57264d5d2bd5caf0e3727b78d7a8dea662dee05
Steven Hedges <steven.hedges34@gmail.com>
2026-09-09 23:55:47 -0400

    Implement Triangle wave channel functionality

--- src/core/apu/Triangle.cpp
+++ src/core/apu/Triangle.cpp
@@ -1,5 +1,78 @@
 #include "Triangle.h"
 
+#include "ApuConstants.h"
+
 namespace nes {
+
+void Triangle::SetEnabled(const bool enabled) {
+  enabled_ = enabled;
+  if (!enabled_)
+    length_counter_ = 0;
+}
+
+// $4008: CRRR RRRR
+void Triangle::WriteLinearCounter(const uint8_t value) {
+  control_flag_ = (value & 0x80) != 0;
+  linear_reload_ = value & 0x7F;
+}
+
+// $400A: TTTT TTTT
+void Triangle::WriteTimerLow(const uint8_t value) {
+  timer_period_ = (timer_period_ & 0x0700) | static_cast<uint16_t>(value);
+}
+
+// $400B: LLLL LTTT
+void Triangle::WriteTimerHigh(const uint8_t value) {
+  timer_period_ = (timer_period_ & 0x00FF) | (static_cast<uint16_t>(value & 0x07) << 8);
+  if (enabled_)
+    length_counter_ = ApuConstants::LENGTH_COUNTER_TABLE[(value >> 3) & 0x1F];
+  linear_reload_flag_ = true;
+}
+
+// Clocked every CPU cycle.
+void Triangle::ClockTimer() {
+  if (timer_ == 0) {
+    timer_ = timer_period_;
+    if (length_counter_ > 0 && linear_counter_ > 0) {
+      sequence_position_ = (sequence_position_ + 1) & 0x1F;
+    }
+  } else {
+    --timer_;
+  }
+}
+
+/**
+ * Clocks every quarter frame.
+ * If the reload flag is set, load the reload value.
+ */
+void Triangle::ClockLinearCounter() {
+  if (linear_reload_flag_) {
+    linear_counter_ = linear_reload_;
+  } else if (linear_counter_ > 0) {
+    --linear_counter_;
+  }
+  if (!control_flag_) {
+    linear_reload_flag_ = false;
+  }
+}
+
+void Triangle::ClockLengthCounter() {
+  if (!control_flag_ && length_counter_ > 0) {
+    --length_counter_;
+  }
+}
+
+uint8_t Triangle::Output() const {
+  if (!enabled_)
+    return 0;
+  if (length_counter_ == 0)
+    return 0;
+  if (linear_counter_ == 0)
+    return 0;
+  if (timer_period_ < 2)
+    return 0; // Silence ultrasonic frequencies
+
+  return SEQUENCE_TABLE[sequence_position_];
+}
 
 } // namespace nes
