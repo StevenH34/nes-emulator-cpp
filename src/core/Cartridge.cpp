ed0af34300eb9f618e1a7a1b6032b1784d0eb19b
Steven Hedges <steven.hedges34@gmail.com>
2026-09-18 23:22:41 -0400

    Add serialization and deserialization methods to Cartridge class

--- src/core/Cartridge.cpp
+++ src/core/Cartridge.cpp
@@ -112,4 +112,12 @@ void Cartridge::Parse(std::span<const uint8_t> data) {
   mapper_ = Mapper::Create(mapper_id_, prg_rom_, chr_rom_);
 }
 
+void Cartridge::Serialize(StateWriter& writer) const {
+  mapper_->Serialize(writer);
+}
+
+void Cartridge::Deserialize(StateReader& reader) {
+  mapper_->Deserialize(reader);
+}
+
 } // namespace nes
